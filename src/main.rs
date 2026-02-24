a91098b8abe493ce47e234f64fc7379ab5f48ea1
Erik Peteri <peteri.erik@gmail.com>
2026-02-24 19:27:48 +0200

    create method to get center coordinates of boid struct

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ use macroquad::prelude::*;
 struct Boid {
     v1: Vec2,
     v2: Vec2,
-    v3: Vec2
+    v3: Vec2,
 }
 
-#[macroquad::main("Ball")]
+impl Boid {
+    fn get_center(&self) -> (f32, f32) {
+        let x = (self.v1.x + self.v2.x + self.v3.x) / 3.0;
+        let y = (self.v1.y + self.v2.y + self.v3.y) / 3.0;
+        (x, y)
+    }
+}
+
+#[macroquad::main("Boid simulation")]
 async fn main() {
 
     let (screen_center_x, screen_center_y) = (screen_width() / 2.0, screen_height() / 2.0);
@@ -15,7 +23,7 @@ async fn main() {
     let mut boid = Boid {
         v1: vec2(screen_center_x - size, screen_center_y + size * multiplier),
         v2: vec2(screen_center_x, screen_center_y),
-        v3: vec2(screen_center_x + size, screen_center_y + size * multiplier)
+        v3: vec2(screen_center_x + size, screen_center_y + size * multiplier),
     };
 
     loop {
@@ -23,6 +31,8 @@ async fn main() {
         
         let (mouse_x, mouse_y) = mouse_position();
         draw_triangle(boid.v1, boid.v2, boid.v3, WHITE);
+        boid.v2 = Vec2{x: mouse_x, y: mouse_y};
+        
         next_frame().await
     }
 }
